7c8656d39336ae4ef16e27f21937ff599237806e
Isaac Brist <isaacbrist@gmail.com>
2022-06-02 15:26:28 -0500

    Completed #2 and 3

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -11,11 +11,12 @@ console.log('***** Object Practice *****')
 const me = {
   // TODO - add properties here
   firstName: 'Isaac',
-  lastName: 'Brist',
+  lastName: ' Brist',
   hasSiblings: true,
   shoeCount: 3,
   favThreeFoods: ['Tom Yum', 'Fresh Salsa', 'Curry'],
 };
+//Testing object 'me'
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
@@ -24,14 +25,16 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
-
+let fullName = me.firstName + me.lastName;
+//testing fullName
+console.log(fullName)
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
+console.log('My first favorite food is:', me.favThreeFoods[0])
+console.log('My last favorite food is:', me.favThreeFoods[me.favThreeFoods.length -1])
 
 
 /* 4. Change a property of an existing object.
